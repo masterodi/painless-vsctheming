a59a6a88c34f8c7435f8d8e62ab0d270224acbb2
masterodi <ovddrg@gmail.com>
2024-11-12 09:20:33 +0200

    feat: Update code

--- src/utils/Theme.ts
+++ src/utils/Theme.ts
@@ -1,70 +1,75 @@
 import Color from "./Color";
 import {
-  createTokenColors,
-  createWorkbenchColors,
-  TokenColorsScheme,
-  VSCTheme,
-  WorkbenchColorsScheme,
+	createTokenColors,
+	createWorkbenchColors,
+	TokensColorsScheme,
+	VSCTheme,
+	WorkbenchColorsScheme,
 } from "./schemas";
 
-type FromBaseProps = {
-  background: string;
-  foreground: string;
-  accent: string;
-  border: string;
-  tokens: TokenColorsScheme;
-};
-
 export default class Theme {
-  name: string;
-  workbench: WorkbenchColorsScheme;
-  tokens: TokenColorsScheme;
+	name: string;
+	workbench: WorkbenchColorsScheme;
+	tokens: TokensColorsScheme;
 
-  constructor(
-    name: string,
-    workbench: WorkbenchColorsScheme,
-    tokens: TokenColorsScheme
-  ) {
-    this.name = name;
-    this.workbench = workbench;
-    this.tokens = tokens;
-  }
+	constructor({
+		name,
+		workbench,
+		tokens,
+	}: {
+		name: string;
+		workbench: WorkbenchColorsScheme;
+		tokens: TokensColorsScheme;
+	}) {
+		this.name = name;
+		this.workbench = workbench;
+		this.tokens = tokens;
+	}
 
-  getName() {
-    return `${this.name.replace(" ", "-")}-color-theme.json`;
-  }
+	getName() {
+		return `${this.name.replace(" ", "-")}-color-theme.json`;
+	}
 
-  build(minimal = true): VSCTheme {
-    return {
-      name: this.name,
-      colors: createWorkbenchColors(this.workbench, minimal),
-      tokenColors: createTokenColors(this.tokens),
-      semanticHighlighting: true,
-    };
-  }
+	build(minimal = true): VSCTheme {
+		return {
+			name: this.name,
+			colors: createWorkbenchColors(this.workbench, minimal),
+			tokenColors: createTokenColors(this.tokens),
+			semanticHighlighting: true,
+		};
+	}
 
-  static fromBases(name: string, props: FromBaseProps) {
-    const theme = new Theme(
-      name,
-      {
-        background: new Color({
-          base: {
-            hex: props.background,
-            brightnessStep: { brighten: 0.2, darken: 0.1 },
-          },
-        }),
-        foreground: new Color({
-          base: { hex: props.foreground, brightnessStep: { darken: 1.2 } },
-        }),
-        accent: new Color({ base: { hex: props.accent } }),
-        border: props.border,
-      },
-      props.tokens
-    );
+	static fromBases(
+		name: string,
+		props: {
+			background: string;
+			foreground: string;
+			accent: string;
+			border: string;
+			tokens: TokensColorsScheme;
+		},
+	) {
+		const theme = new Theme({
+			name,
+			workbench: {
+				background: new Color({
+					base: {
+						hex: props.background,
+						brightnessStep: { brighten: 0.2, darken: 0.1 },
+					},
+				}),
+				foreground: new Color({
+					base: { hex: props.foreground, brightnessStep: { darken: 1.2 } },
+				}),
+				accent: new Color({ base: { hex: props.accent } }),
+				border: props.border,
+			},
+			tokens: props.tokens,
+		});
 
-    console.log("Workbench colors:");
-    console.table(theme.workbench);
+		console.log("Workbench colors:");
+		console.table(theme.workbench);
 
-    return theme;
-  }
+		return theme;
+	}
 }
